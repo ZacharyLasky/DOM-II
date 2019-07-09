b90d90e9c42d5764c45b9825620d9f8b69eb02a0
Zach Lasky <zacharylasky123@gmail.com>
2019-07-09 12:37:35 -0400

    started strecth goal

---
+++ stretch-assignment/index.js
@@ -0,0 +1,7 @@
+//SELECT INDIVIDUAL CLASSES
+const blockRed = document.querySelector(".block--red");
+const blockBlue = document.querySelector(".block--blue");
+const blockGreeb = document.querySelector(".block--green");
+const blockPink = document.querySelector(".block--pink");
+const blockGray = document.querySelector(".block--gray");
+console.log(blockGray);
